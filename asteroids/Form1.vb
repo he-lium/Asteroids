707f8d88d56d36cb8d7c65061517ce3b452e1209
Kevin <hihellohi610@gmail.com>
2014-11-06 17:41:40 +1100

    made code more elegant

--- asteroids/Form1.vb
+++ asteroids/Form1.vb
@@ -8,18 +8,17 @@
     Const MAX_ASTEROID_SIZE As Integer = 100
     Const GRAVITY As Integer = 4000
 
-    Private vx As Double = 0
-    Private vy As Double = 0
-    Private x As Double
-    Private y As Double
     Private direction As Double = Math.PI * 0.5
     Private degrees As Integer = 90
     Dim upkey = False, leftkey = False, rightkey = False
     Private asteroids(NUM_ASTEROIDS) As asteroid
+    Private spaceship As asteroid
 
     Private Structure asteroid
-        Public vx As Integer
-        Public vy As Integer
+        Public x As Double
+        Public y As Double
+        Public vx As Double
+        Public vy As Double
         Public size As Integer
         Public picture As System.Windows.Forms.PictureBox
     End Structure
@@ -51,31 +50,28 @@
         degrees = truemod(direction * (180 / Math.PI), 360)
         Me.Text = degrees
         If upkey Then
-            vy -= Math.Sin(direction) * ACCELERATION
-            vx += Math.Cos(direction) * ACCELERATION
+            spaceship.vy -= Math.Sin(direction) * ACCELERATION
+            spaceship.vx += Math.Cos(direction) * ACCELERATION
         End If
-        Dim dist As Double = GRAVITY / Math.Max(((y - ((PictureBox2.Top + PictureBox2.Bottom) / 2)) ^ 2) + ((x - ((PictureBox2.Right + PictureBox2.Left) / 2)) ^ 2), 1)
-        Dim deg As Double = Math.Atan2((y - ((PictureBox2.Top + PictureBox2.Bottom) / 2)), (x - ((PictureBox2.Right + PictureBox2.Left) / 2)))
-        vy -= Math.Sin(deg) * dist
-        vx -= Math.Cos(deg) * dist
-        vx = Math.Min(MAX_SPEED, vx)
-        vy = Math.Min(MAX_SPEED, vy)
-        y += vy
-        y = truemod(y, Me.Bottom - Me.Top)
-        x += vx
-        x = truemod(x, Me.Right - Me.Left)
-        PictureBox1.Left = x - PictureBox1.Width / 2
-        PictureBox1.Top = y - PictureBox1.Height / 2
+        Dim dist As Double = GRAVITY / Math.Max(((spaceship.y - ((PictureBox2.Top + PictureBox2.Bottom) / 2)) ^ 2) + ((spaceship.x - ((PictureBox2.Right + PictureBox2.Left) / 2)) ^ 2), 1)
+        Dim deg As Double = Math.Atan2((spaceship.y - ((PictureBox2.Top + PictureBox2.Bottom) / 2)), (spaceship.x - ((PictureBox2.Right + PictureBox2.Left) / 2)))
+        spaceship.vy -= Math.Sin(deg) * dist
+        spaceship.vx -= Math.Cos(deg) * dist
+        spaceship.vx = Math.Min(MAX_SPEED, spaceship.vx)
+        spaceship.vy = Math.Min(MAX_SPEED, spaceship.vy)
+        move_asteroid(spaceship)
         For i = 0 To NUM_ASTEROIDS - 1
             move_asteroid(asteroids(i))
         Next
     End Sub
 
-    Private Sub move_asteroid(ByVal tmp As asteroid)
-        tmp.picture.Top += tmp.vy
-        tmp.picture.Top = truemod(tmp.picture.Top, Me.Bottom - Me.Top)
-        tmp.picture.Left += tmp.vx
-        tmp.picture.Left = truemod(tmp.picture.Left, Me.Right - Me.Left)
+    Private Sub move_asteroid(ByRef tmp As asteroid)
+        tmp.y += tmp.vy
+        tmp.y = truemod(tmp.y, Me.Bottom - Me.Top)
+        tmp.x += tmp.vx
+        tmp.x = truemod(tmp.x, Me.Right - Me.Left)
+        tmp.picture.Left = tmp.x - tmp.picture.Width / 2
+        tmp.picture.Top = tmp.y - tmp.picture.Height / 2
     End Sub
 
     Private Sub Form1_KeyUp(sender As Object, e As System.Windows.Forms.KeyEventArgs) Handles Me.KeyUp
@@ -98,15 +94,22 @@
         asteroid1.picture = New PictureBox
         asteroid1.picture.BackColor = Color.HotPink
         asteroid1.picture.Top = Math.Floor((Rnd() * Me.Bottom - Me.Top))
+        asteroid1.y = (asteroid1.picture.Top + asteroid1.picture.Top) / 2
         asteroid1.picture.Left = Math.Floor((Rnd() * Me.Right - Me.Left))
+        asteroid1.x = (asteroid1.picture.Right + asteroid1.picture.Left) / 2
         asteroid1.picture.Size = New Size(size, size)
         Me.Controls.Add(asteroid1.picture)
         Return asteroid1
     End Function
 
     Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
-        x = (PictureBox1.Right + PictureBox1.Left) / 2
-        y = (PictureBox1.Top + PictureBox1.Bottom) / 2
+        spaceship = New asteroid
+        spaceship.picture = PictureBox1
+        spaceship.x = (spaceship.picture.Right + spaceship.picture.Left) / 2
+        spaceship.y = (spaceship.picture.Top + spaceship.picture.Bottom) / 2
+        spaceship.size = 0
+        spaceship.vx = 0
+        spaceship.vy = 0
         Randomize()
         For i = 0 To NUM_ASTEROIDS - 1
             asteroids(i) = make_asteroid(MAX_ASTEROID_SIZE)
